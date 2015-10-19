81440980e13bc15f3bcfc150f82a35774b8ecf5d
matthewly <mmly2@illinois.edu>
2015-10-19 01:51:50 -0500

    added user search query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   end
 
   def index
-      @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   private
