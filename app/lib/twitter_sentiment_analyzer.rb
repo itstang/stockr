ff35ad41641c30f528777d793f52fb66ae1df516
Kevin Ly <kevinhly01@gmail.com>
2015-11-29 14:11:06 -0600

    sentiment for tweets

--- app/lib/twitter_sentiment_analyzer.rb
+++ app/lib/twitter_sentiment_analyzer.rb
@@ -3,14 +3,28 @@ require '../../config/initializers/alchemyapi'
 
 alchemyapi = AlchemyAPI.new()
 
-myText = "I can't wait to integrate AlchemyAPI's awesome Ruby SDK into my app!"
-response = alchemyapi.sentiment("text", myText)
-puts "Sentiment: " + response["docSentiment"]["type"]
+# myText = "I can't wait to integrate AlchemyAPI's awesome Ruby SDK into my app!"
+# response = alchemyapi.sentiment("text", myText)
+# puts "Sentiment Score: " + response["docSentiment"]["score"] + " Sentiment type: " + response["docSentiment"]["type"]
 
-tweets = $twitter.search('$' + 'XOM' + ' -rt',
+tweets = $twitter.search('$' + 'AAPL' + ' -rt',
                                result_type: 'mixed',
                                count: 20).take(100)
 
-tweets.each do |tweet|
-	puts tweet.text
+tweets.uniq
+new_tweets_arr = Array.new
+tweets.each do |tweet|	
+	tweet_no_url= tweet.text.dup
+	tweet_no_url.gsub!(/(?:f|ht)tps?:\/[^\s]+/, '')
+	new_tweets_arr.push(tweet_no_url)
+end
+
+# remove duplicates
+new_tweets_arr.uniq!
+new_tweets_arr.each do |tweet|	
+	tweet_sentiment= alchemyapi.sentiment("text", tweet)
+	puts tweet
+	if tweet_sentiment["status"] == 'OK'
+		puts "Sentiment Score: " + tweet_sentiment["docSentiment"]["score"] + " Sentiment type: " + tweet_sentiment["docSentiment"]["type"]
+	end
 end
