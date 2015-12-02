837508509f0a51a045652964eb036adce5becdfe
matthewly <mmly2@illinois.edu>
2015-12-01 20:29:19 -0600

    added stock boards

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -29,49 +29,8 @@ class StaticPagesController < ApplicationController
 
   def stocks_show
     @stock = Stock.find(params[:id])
-    yahoo_client = YahooFinance::Client.new
-    @h = historical_data = yahoo_client.historical_quotes(@stock.symbol, { start_date: Time::now-(24*60*60*21), end_date: Time::now })
-    #@graph= histogram(@stock.symbol)
   end
 
-  def histogram(stock_symbol)
-    yahoo_client = YahooFinance::Client.new
-    @h = historical_data = yahoo_client.historical_quotes(stock_symbol, { start_date: Time::now-(24*60*60*21), end_date: Time::now })
-
-    graph = Gruff::Line.new(600)
-    graph.title = 'Three Week History of ' + stock_symbol
-
-    x_axis = []
-
-    historical_data.each { |row|  x_axis << row['trade_date'] }
-    start_date = x_axis.min
-    middle_date = x_axis[(x_axis.length ) / 2]
-    end_date = x_axis.max
-
-    graph.labels = { 0 => start_date, 7 => middle_date, 13 => end_date }
-
-    open_history = Array.new
-    close_history = Array.new
-    high_history = Array.new
-    low_history = Array.new
-
-    historical_data.each do |date_data| 
-      open_history.push(date_data['open'].to_f)
-      close_history.push(date_data['close'].to_f)
-      high_history.push(date_data['high'].to_f)
-      low_history.push(date_data['low'].to_f)
-    end
-
-    graph.data('Open', open_history, '#B75582')
-    graph.data('High', high_history, '#79C65B' )
-    graph.data('Low', low_history)
-    graph.data('Close', close_history)
-
-    graph.x_axis_label = 'Date'
-    graph.y_axis_label = 'Dollars'
-
-    send_data(graph.to_blob, :filename => "stock.png", :type => 'image/png', :disposition=> 'inline')
-  end
 
   def stocks_add
     User_Owns.create(email: current_user.email, symbol: params[:symbol])
